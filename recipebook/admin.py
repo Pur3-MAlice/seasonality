140af4f48e441b557335f4ad1dd9ea222afbdc3b
Pur3-MAlice <aliceford98@live.co.uk>
2023-09-01 20:57:17 +0000

    adds basic base, index and recipe_detail template to templates folder

--- recipebook/admin.py
+++ recipebook/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Recipe, Diet
+from .models import Recipe, Diet, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -8,8 +8,19 @@ admin.site.register(Diet)
 
 @admin.register(Recipe)
 class RecipeAdmin(SummernoteModelAdmin):
-    repopulated_fields = {'slug': ('title',)}
+    prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on', 'diet')
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     summernote_fields = ('content', 'ingredients')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ('approved', 'created_on')
+    list_display = ('name', 'body', 'approved', 'created_on', 'recipe')
+    search_fields = ['name', 'body']
+    actions = ['close_comments']
+
+    def close_comments(Self, request, queryset):
+        queryset.update(approved=False)
