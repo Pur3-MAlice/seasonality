8e586c12f134088726882f43b392b260ef8f3337
Pur3-MAlice <aliceford98@live.co.uk>
2023-09-04 15:06:55 +0000

    links up profile tempalte with base link

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# Extending User Model Using a One-To-One Link
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = CloudinaryField('image', default='placeholder')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
 
+
 name = 'profiles_urls'
 
 urlpatterns = [
-    
+    path('profile/', views.profile, name='profile'),
 ]
