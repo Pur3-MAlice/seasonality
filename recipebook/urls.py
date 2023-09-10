5aadbfc12dfcebdac4e90443fd8c0af5e7692b53
Pur3-MAlice <aliceford98@live.co.uk>
2023-09-10 15:12:09 +0000

    moves html templates into apps for self contained function

--- recipebook/urls.py
+++ recipebook/urls.py
@@ -5,11 +5,11 @@ name = 'recipebook_urls'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='home'),
-    path('add_recipe/', views.add_recipe, name="add_recipe"),
-    path('update_recipe/<recipe_id>/', views.update_recipe, name="update_recipe"),
-    path('delete_recipe/<recipe_id>/', views.delete_recipe, name='delete_recipe'),
-    path('delete_comment/<comment_id>/', views.delete_comment, name='delete_comment'),
-    path('search_results/', views.search_results, name='search_results'),
-    path('<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
+    path('recipebook/add_recipe/', views.add_recipe, name="add_recipe"),
+    path('recipebook/update_recipe/<recipe_id>/', views.update_recipe, name="update_recipe"),
+    path('recipebook/delete_recipe/<recipe_id>/', views.delete_recipe, name='delete_recipe'),
+    path('recipebook/delete_comment/<comment_id>/', views.delete_comment, name='delete_comment'),
+    path('recipebook/search_results/', views.search_results, name='search_results'),
+    path('recipebook/<slug:slug>/', views.RecipeDetail.as_view(), name='recipe_detail'),
     path('rate/<int:recipe_id>/<int:rating>/', views.rate),
 ]
--- recipebook/views.py
+++ recipebook/views.py
@@ -39,7 +39,7 @@ def add_recipe(request):  # Code inspired by Codemy.com
             recipe.status = 0
             recipe.author = request.user
             form.save()
-            return HttpResponseRedirect('/add_recipe?submitted=True')
+            return HttpResponseRedirect('/recipebook/add_recipe?submitted=True')
         else:
             messages.error(request, "Error")
     else:
