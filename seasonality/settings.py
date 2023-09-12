a6650c717d5eb744db666b673820b67774413725
Pur3-MAlice <aliceford98@live.co.uk>
2023-09-12 22:36:51 +0000

    edits the settings

--- seasonality/settings.py
+++ seasonality/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEV")
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-pur3malice-seasonality-h54p49my3wq.ws-eu104.gitpod.io',
@@ -108,17 +108,9 @@ WSGI_APPLICATION = 'seasonality.wsgi.application'
 #     }
 # }
 
-if 'DEV' in os.environ:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
-            'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
