4cf1acba3ae79c5a313788197f1c2a91bed5b16d
Pur3-MAlice <aliceford98@live.co.uk>
2023-09-12 20:23:30 +0000

    edits css for validator - border color and color the same

--- seasonality/urls.py
+++ seasonality/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+# Main Url Patterns, pulls from both apps
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
